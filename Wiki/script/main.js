dbee8d9e50bc64c829ebf998f3d13b97774b22ae
Emmanuel chukwurah <emekaemmanuel045@gmail.com>
2020-11-30 16:30:36 +0100

    Update main.js

--- Wiki/script/main.js
+++ Wiki/script/main.js
@@ -18,7 +18,7 @@ async function searchTerm() {
   //query for the api
   queryTerm = queryTerm.split(" ").join("_");
   const url =
-    "http://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=%27" +
+    "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=%27" +
     queryTerm;
 
   //fetch data
